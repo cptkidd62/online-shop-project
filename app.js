8906f906af370dfe14e4cea1ec46f03f6378707b
cptkidd62 <zelinka15@gmail.com>
2023-01-03 13:56:10 +0100

    add basic css

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+app.use(express.static("static"));
+
 app.get("/", (req, res) => {
     res.render("index");
 });
